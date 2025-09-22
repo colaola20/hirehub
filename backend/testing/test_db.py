eb5a243e3f10c5ee00ef8cad7f1dd6128b67e897
colaola20 <soryo@farmingdale.edu>
2025-09-21 22:24:00 -0400

    Cleaned unneeded files and added new dependencies to requirements

--- backend/testing/test_db.py
+++ backend/testing/test_db.py
@@ -5,11 +5,13 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from app import create_app, db
 from sqlalchemy import text
 
-app = create_app()
+def test_database_connection():
 
-with app.app_context():
-    try:
-        db.session.execute(text('SELECT 1'))
-        print("Database connection successful!")
-    except Exception as e:
-        print("Database connection failed:", e)
+    app = create_app()
+
+    with app.app_context():
+        try:
+            db.session.execute(text('SELECT 1'))
+            print("Database connection successful!")
+        except Exception as e:
+            print("Database connection failed:", e)
