8c9a1522db703ca489b696fd6a4c403aaa0c7c20
HarisAkbar03 <ha.akbar03@gmail.com>
2025-10-28 00:49:19 -0400

    all the final changes

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -18,7 +18,7 @@ from app.routes.skills import skills_bp
 from app.routes.profile import profile_bp
 from app.routes.documents import documents_bp
 
-
+from app.routes.chat_bot import chat_bp
 from flask_cors import CORS
 from .config import Config
 
@@ -84,6 +84,7 @@ def create_app():
     app.register_blueprint(linkedin_bp)
     app.register_blueprint(applications_bp)
     app.register_blueprint(documents_bp)
-    
+    app.register_blueprint(chat_bp, url_prefix="/api") 
+
 
     return app
