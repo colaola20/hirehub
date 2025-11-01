bbfe835c4a4e3a30964c13b729291e8bc09d82fd
colaola20 <soryo@farmingdale.edu>
2025-10-31 23:32:21 -0400

    Started to work on Settings page

--- frontend/frontend/src/pages/Setting.jsx
+++ frontend/frontend/src/pages/Setting.jsx
@@ -1,5 +1,13 @@
+import styles from './Setting.module.css'
+
 const Settings = () => {
 
+    return (
+        <div className={styles.settingContainer}>
+            <h3>Login & Security</h3>
+            <div className={styles.separator}></div>
+        </div>
+    )
 }
 
 export default Settings
