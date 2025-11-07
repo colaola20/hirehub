c99983a6e14392a76edd5d5fbd756d4d513eebd0
colaola20 <soryo@farmingdale.edu>
2025-11-06 22:14:42 -0500

    Improved forgot password modal and made some style fixes.

--- frontend/frontend/src/components/settingPage/PasswordReset.jsx
+++ frontend/frontend/src/components/settingPage/PasswordReset.jsx
@@ -1,7 +1,13 @@
+import { BiKey } from 'react-icons/bi'
+import styles from './PasswordReset.module.css'
+
 const PasswordReset = () => {
     return (
-        <div className={StyleSheet.modalContainer}>
-            <h2>Forgot Password?</h2>
+        <div className={styles.overlay}>
+            <div className={styles.modalContainer}>
+                <BiKey />
+                <h2>Forgot Password?</h2>
+            </div>
         </div>
     )
 }
--- frontend/frontend/src/pages/Setting.jsx
+++ frontend/frontend/src/pages/Setting.jsx
@@ -45,29 +45,31 @@ const Settings = () => {
     }
 
     return (
-        <div className={styles.settingContainer}>
-            <div className={styles.loginSection}>
-                <h3 className={styles.title}>Login & Security</h3>
-                <div className={styles.separator}></div>
-                <div className={styles.email}>
-                    <h5 className={styles.label}>Email</h5>
-                    <p>{email}</p>
-                </div>
-                <div className={styles.password} >
-                    <h5 className={styles.label}>Password</h5>
-                    <button className={styles.setBtn} onClick={handleResetPassword}>Reset password</button>
-                </div>
-                <div className={styles.deleteAccount}>
-                    <div>
-                        <h5 className={styles.label}>Delete my acount</h5>
-                        <p>Permanently delete your HireHub account and all associated data</p>
+        <div className={styles.container}>
+            <div className={styles.settingContainer}>
+                <div className={styles.loginSection}>
+                    <h3 className={styles.title}>Login & Security</h3>
+                    <div className={styles.separator}></div>
+                    <div className={styles.email}>
+                        <h5 className={styles.label}>Email</h5>
+                        <p>{email}</p>
                     </div>
-                    <button className={styles.setBtn}>Delete my account</button>
+                    <div className={styles.password} >
+                        <h5 className={styles.label}>Password</h5>
+                        <button className={styles.setBtn} onClick={handleResetPassword}>Reset password</button>
+                    </div>
+                    <div className={styles.deleteAccount}>
+                        <div>
+                            <h5 className={styles.label}>Delete my acount</h5>
+                            <p>Permanently delete your HireHub account and all associated data</p>
+                        </div>
+                        <button className={styles.setBtn}>Delete my account</button>
+                    </div>
                 </div>
-            </div>
-            <div className={styles.alertsPreference}>
-                <h3 className={styles.title}>Job Alerts Preference</h3>
-                <div className={styles.separator}></div>
+                <div className={styles.alertsPreference}>
+                    <h3 className={styles.title}>Job Alerts Preference</h3>
+                    <div className={styles.separator}></div>
+                </div>
             </div>
             {isOpenPasswordReset && (
                 <PasswordReset />
