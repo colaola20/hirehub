648617f782603463eeccccecd93fb8a1ca82290a
colaola20 <soryo@farmingdale.edu>
2025-11-09 13:39:41 -0500

    Added Success and Error modals for user's action feedback. They need to improved. Additionally, added hover style for SENT button in password reset.

---
+++ frontend/frontend/src/components/UsersMessages/Error.jsx
@@ -0,0 +1,12 @@
+import styles from './Error.module.css'
+const Error = () => {
+    return (
+        <div className={styles.overlay}>
+            <div>
+                <h1>Something went wrong</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Error
---
+++ frontend/frontend/src/components/UsersMessages/Success.jsx
@@ -0,0 +1,12 @@
+import styles from './Success.module.css'
+const Success = () => {
+    return (
+        <div className={styles.overlay}>
+            <div>
+                <h1>All good</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Success
