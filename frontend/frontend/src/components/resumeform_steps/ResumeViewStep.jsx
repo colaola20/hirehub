0f7f71f8b6405eab8640c38f0962f4a6c61510aa
hubao <kazmha@farmingdale.edu>
2025-12-01 21:24:24 -0500

    download button functionality

--- frontend/frontend/src/components/resumeform_steps/ResumeViewStep.jsx
+++ frontend/frontend/src/components/resumeform_steps/ResumeViewStep.jsx
@@ -2,15 +2,36 @@ import React, { useEffect, useState } from 'react';
 import style from './resumeview.module.css'
 import Btn from '../buttons/Btn'
 import ResumeTemplate from './ResumeTemplate';
+import html2pdf from "html2pdf.js";
+
 
 const ResumeViewStep = ({ backendData }) => {
 
+
+    const pdfDL = () => {
+        const element = document.getElementById("resume-container")
+
+        if (!element){
+            console.error("Resume container not found!");
+            return;
+        }
+
+        const options = {
+            margin: 0,
+            filename: 'resume.pdf',
+            image: {type: 'jpeg', quality: 0.98},
+            html2canvad: {scale: 2},
+            jsPDF: {unit: 'in', format: 'letter', orientation: 'portrait'}
+        }
+
+        html2pdf().set(options).from(element).save();
+    }
 
     return (
         <div>
             <h2>Resume Preview</h2> {/* DISPLAY JSON if available */}
                 <div className={style.resumeScaler}>
-                    <div className={style['resume-preview']}>
+                    <div className={style['resume-preview']} id="resume-container">
                         {/* <pre>{JSON.stringify(backendData, null, 2)}</pre> */}
                         {backendData.aiResumeText ? 
                         (
@@ -26,7 +47,7 @@ const ResumeViewStep = ({ backendData }) => {
             <div className={style.viewBtn}>
                 <Btn label={"Open Resume In New Tab"}/>
                 <Btn label={"Save To Document Storage"}/>
-                <Btn label={"Download Resume"}/>
+                <Btn label={"Download Resume"} onClick={pdfDL}/>
             </div>
 
         </div>
