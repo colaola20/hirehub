39d68946f7c2507e7b56fb4a62e4b6832b486538
PracticalEscapement <themoradis1@gmail.com>
2025-12-04 23:14:49 -0500

    Intial setup for docker config and build

--- frontend/frontend/vite.config.js
+++ frontend/frontend/vite.config.js
@@ -8,5 +8,13 @@ export default defineConfig({
     proxy: {
       '/api': 'http://127.0.0.1:5001',
     }
+  },
+  build: {
+    rollupOptions: {
+      external: [/^vanta/],
+      output: {
+        manualChunks: undefined
+      }
+    }
   }
 })
